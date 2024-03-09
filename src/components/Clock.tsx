5e9530dbad84032be6c82a49768859a08bb6b152
Saad A <saadahmad96@hotmail.com>
2024-03-09 23:11:07 +0500

    fix: clock hydration warning

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,7 +6,7 @@ import { useInterval } from "usehooks-ts";
 export default function Clock() {
   const now = useCurrentTime();
   return (
-    <div className="fixed top-4 right-4">
+    <div className="fixed top-4 right-4" suppressHydrationWarning>
       {dayjs(now).format("DD-MM-YYYY : hh:mm:ss")}
     </div>
   );
